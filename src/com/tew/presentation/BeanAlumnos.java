1feffa694ede858055718925aa35dad2705e47cc
Daslep <Daslep@DESKTOP-OUBIA0N>
2019-09-30 15:24:32 +0200

    TEW - 3.3 Managed Bean BeanAlumnos implemementación del método Baja

--- src/com/tew/presentation/BeanAlumnos.java
+++ src/com/tew/presentation/BeanAlumnos.java
@@ -9,20 +9,20 @@ import com.tew.model.Alumno;
 @ManagedBean
 @ApplicationScoped
 public class BeanAlumnos implements Serializable{
-	
+
 	private static final long serialVersionUID = 55555L;
-	
+
 	// Se añade este atributo de entidad para recibir el alumno concreto seleccionado
 	// de la tabla o de un formulario.
 	// Es necesario inicializarlo para que al entrar desde el formulario de
 	// AltaForm.xhtml se puedan dejar los valores en un objeto existente.
 	private Alumno alumno = new Alumno();
 	private Alumno[] alumnos = null;
-	
+
 	public BeanAlumnos(){
 		iniciaAlumno(null);
 	}
-	
+
 	public void iniciaAlumno(ActionEvent event) {
 		alumno.setId(null);
 		alumno.setIduser("IdUser");
@@ -46,6 +46,79 @@ public class BeanAlumnos implements Serializable{
 	public void setAlumnos(Alumno[] alumnos) {
 		this.alumnos = alumnos;
 	}
-	
-	
+
+	public String listado() {
+		AlumnosService service;
+		try {
+			// Acceso a la implementacion de la capa de negocio
+			// a través de la factoría
+			service = Factories.services.createAlumnosService();
+			// Asi le damos información a toArray para poder hacer el casting a Alumno[]
+			alumnos = (Alumno [])service.getAlumnos().toArray(new Alumno[0]);
+			return "exito";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "error";
+		}
+	}
+
+	public String edit() {
+		AlumnosService service;
+		try {
+			// Acceso a la implementacion de la capa de negocio
+			// a través de la factoría
+			service = Factories.services.createAlumnosService();
+			//Recargamos el alumno en la tabla de la base de datos por si hubiera cambios.
+			alumno = service.findById(alumno.getId());
+			return "exito";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "error";
+		}
+	}
+
+	public String salva() {
+		AlumnosService service;
+		try {
+			// Acceso a la implementacion de la capa de negocio
+			// a través de la factoría
+			service =  Factories.services.createAlumnosService();
+			//Salvamos o actualizamos el alumno segun sea una operacion de alta o de edición
+			if (alumno.getId() == null) {
+				service.saveAlumno(alumno);
+			}
+			else {
+				service.updateAlumno(alumno);
+			}
+			//Actualizamos el javabean de alumnos inyectado en la tabla
+			alumnos = (Alumno [])service.getAlumnos().toArray(new Alumno[0]);
+			return "exito";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "error";
+		}
+	}
+
+	public String baja() {
+		AlumnosService service;
+		try {
+			// Acceso a la implementacion de la capa de negocio
+			// a través de la factoría
+			service =  Factories.services.createAlumnosService();
+			//Salvamos o actualizamos el alumno segun sea una operacion de alta o de edición
+			if (alumno.getId() != null) {
+				service.deleteAlumno(alumno.getId());
+				//Actualizamos el javabean de alumnos inyectado en la tabla
+				alumnos = (Alumno [])service.getAlumnos().toArray(new Alumno[0]);
+				return "exito";
+			}
+			else {
+				return "error";
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "error";
+		}
+	}
 }
